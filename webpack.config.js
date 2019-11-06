9088e69c3447adea244b1a3fd3e70092d379a2e9
Sam Slotsky <sam@manifold.co>
2019-11-06 13:09:21 -0600

    Cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "dist/"
+    filename: "[name].[contenthash].js"
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "My Marketplace",
+      template: "index.html",
+      inject: "head"
+    })
+  ],
   module: {
     rules: [
       {
