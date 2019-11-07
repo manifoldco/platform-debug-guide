18fe7f94f3bc2ea46e8f0fe79c098050f9883d0e
Sam Slotsky <sam@manifold.co>
2019-11-07 07:34:49 -0600

    Generate source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -9,12 +11,23 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js"
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true
+      })
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "My Marketplace",
       template: "index.html",
       inject: "head"
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[name].map"
     })
   ],
   module: {
